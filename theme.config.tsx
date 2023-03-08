76cacc4816b3e34dd9482ba686e303c919569d81
Elmer Chanquin <elmer@em.gt>
2023-03-08 10:41:30 -0600

    Update links on theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Unified Access Technology of Elements and Materials</span>,
   project: {
-    link: 'https://github.com/elmerchanquin/uatem/README.md',
+    link: 'https://github.com/elmerchanquin/uatem',
   },
   chat: {
     link: 'https://discord.gg/YK6NbQzk27',
   },
-  docsRepositoryBase: 'https://github.com/elmerchanquin/uatem',
+  docsRepositoryBase: 'https://github.com/elmerchanquin/uatem/blob/main/README.md',
   footer: {
     text: 'UATEM',
   },
