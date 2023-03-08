4e52530046f76943bcd5cf60d428910c42ffa36d
Elmer Chanquin <elmer@em.gt>
2023-03-08 10:39:12 -0600

    Update theme data

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Unified Access Technology of Elements and Materials</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/elmerchanquin/uatem/README.md',
   },
   chat: {
-    link: 'https://discord.com/uatem',
+    link: 'https://discord.gg/YK6NbQzk27',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/elmerchanquin/uatem',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'UATEM',
   },
 }
 
